4a4e9cf73a9beb335458a401e849105ccdb5b9b3
Martin Parak <martin.parak@itvkurze.sk>
2017-10-10 21:06:19 +0200

    initial commit

---
+++ src/main/java/sk/mapa/hello/HelloWorldBean.java
@@ -0,0 +1,20 @@
+package sk.mapa.hello;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="helloWorld")
+public class HelloWorldBean {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
